cb3951da2c17df71bf12ce748b06a575e59f47cd
Bisaniz <bisandawabsheh@gmail.com>
2023-11-01 21:57:44 +0200

    first commit

---
+++ module.js
@@ -0,0 +1,11 @@
+// // exporting a function
+// export function greetPerson(name) {
+//     return `Hello ${name}`;
+// }
+
+
+export function sayHello() {
+    console.log("Hello!");
+ }
+ 
+ export const message = "Welcome to the module!";
---
+++ script.js
@@ -0,0 +1,13 @@
+// // importing greetPerson from greet.js file
+// import { greetPerson } from './module.js';
+
+// // using greetPerson() defined in greet.js
+// let displayName = greetPerson('Jack');
+
+// console.log(displayName); // Hello Jack
+
+
+import { sayHello, message } from "./module.js";
+
+sayHello(); // Output: Hello!
+console.log(message); // Output: Welcome to the module!
